30b8a3cf0396daf82088eee65445fd56807f09df
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-04-30 11:24:07 +0530

    create super user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successfull(self):
@@ -15,3 +16,28 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user id normalized"""
+        email = "test@EMAIL.com"
+        user = get_user_model().objects.create_user(
+            email,
+            'test1233'
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def new_user_invalid_email(self):
+        """Test creating a new user no email raises an error"""
+        with self.asertRaises(ValueError):
+            get_user_model.objects.create_user(None, 'test1233')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@email.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
