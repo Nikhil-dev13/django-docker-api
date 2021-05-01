79fc92ecfce9ec5851ecdaf8df3a075abfd5bab2
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-05-01 12:06:03 +0530

    create user create api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -2,11 +2,12 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
-from rest_framework import APIClient
+from rest_framework.test import APIClient
 from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
@@ -17,7 +18,7 @@ class PublicUserApiTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-    
+
     def test_create_valid_user_success(self):
         """Test creating with valid payload is succesful"""
         payload = {
@@ -43,11 +44,11 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that the password must be more than 5 characters"""
-        payload = { 'email': 'test@email.com', 'password': 'pwd'}
+        payload = {'email': 'test@email.com', 'password': 'pwd'}
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
-            email=payload('email')
+            email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
